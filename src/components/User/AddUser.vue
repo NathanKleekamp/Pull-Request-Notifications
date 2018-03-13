ed1e02ec2d609bbfd3f1ae6222d25cbf124561b8
NathanKleekamp <nkleekamp@gmail.com>
2018-03-13 08:59:23 -0400

    add keyboard support when adding user

--- src/components/User/AddUser.vue
+++ src/components/User/AddUser.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <form v-on:submit.prevent="onSubmit" class="add-user-form">
+  <form v-on:submit="onSubmit" class="add-user-form">
     <div class="form-wrapper">
       <label for="github-username">Github Username</label>
       <input id="github-username" type="text" name="github-username" value="">
     </div>
-    <button v-on:click="onSubmit" type="button" name="Submit" class="button">Add User</button>
+    <input type="submit" name="Submit" class="button" value="Add User">
   </form>
 </template>
 
@@ -17,7 +17,7 @@
       onSubmit(event) {
         event.preventDefault();
 
-        const form = event.target.form;
+        const form = event.target;
         const formData = new FormData(form);
         const user = formData.get('github-username').trim().toLowerCase();
 
@@ -31,3 +31,19 @@
     },
   };
 </script>
+
+<style scoped>
+  input[type=submit] {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    background: transparent;
+    border: 1px solid #fff;
+    border-radius: 4px;
+    color: #fff;
+    height: 35px;
+    margin: 0;
+    padding: .5em 1em;
+    position: relative;
+    top: 19px;
+  }
+</style>
